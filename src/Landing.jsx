882dcdfcba793fd433289c77d5f77471f4ef835f
Shreyansh7676 <shreyansh.ind18@gmail.com>
2025-03-30 19:18:45 +0530

    Added Dock to the main page

--- src/Landing.jsx
+++ src/Landing.jsx
@@ -1,63 +1,86 @@
-import React from 'react';
-import { Github, Linkedin, Mail, ExternalLink, ChevronDown, Code2 } from 'lucide-react';
+import React, { useRef } from 'react';
+import { Github, Linkedin, Mail, ExternalLink, ChevronDown, Code2, House, User, Layers, Trophy } from 'lucide-react';
 import Hero from './Hero'
 import About from './About';
 import Projects from './Projects';
-import Orb from './components/Orb';
+import Contact from './Contact';
 import TechStack from './TechStack';
 import Achievements from './Achievements';
+import Dock from './components/Dock'
+import AnimatedContent from './components/AnimatedComponent';
 
 function App() {
+    const contactRef = useRef(null);
+    const projectRef = useRef(null);
+    const homeRef=useRef(null);
+    const techRef=useRef(null);
+    const aboutRef=useRef(null);
+    const achievementsRef=useRef(null);
+
+    const scrollToContact = () => {
+        contactRef.current.scrollIntoView({ behavior: 'smooth' });
+    };
+    const scrollToProject = () => {
+        projectRef.current.scrollIntoView({ behavior: 'smooth' });
+    };
+    const scrollToHome = () => {
+        homeRef.current.scrollIntoView({ behavior: 'smooth' });
+    };
+    const scrollToAbout = () => {
+        aboutRef.current.scrollIntoView({ behavior: 'smooth' });
+    };
+    const scrollToTech = () => {
+        techRef.current.scrollIntoView({ behavior: 'smooth' });
+    };
+    const scrollToAchievements = () => {
+        achievementsRef.current.scrollIntoView({ behavior: 'smooth' });
+    };
+
+    const items = [
+
+        { icon: <House size={18} className='text-emerald-400' />, label: 'Home', onClick:scrollToHome },
+
+        { icon: <User size={18} className='text-emerald-400' />, label: 'About', onClick:scrollToAbout },
+
+        { icon: <Code2 size={18} className='text-emerald-400' />, label: 'Project', onClick:scrollToProject },
+
+        { icon: <Layers size={18} className='text-emerald-400' />, label: 'Tech Stack', onClick:scrollToTech },
+        { icon: <Trophy size={18} className='text-emerald-400' />, label: 'Experience', onClick:scrollToAchievements},
+        { icon: <Mail size={18} className='text-emerald-400' />, label: 'Contact', onClick:scrollToContact },
+
+    ];
+
     return (
         <div className="bg-zinc-950 text-gray-100 min-h-screen">
             <Hero />
-            <div className="absolute bottom-10 left-1/2 transform -translate-x-1/2 animate-bounce">
+
+            <div className="absolute bottom-30 left-1/2 transform -translate-x-1/2 animate-bounce">
                 <ChevronDown className="w-6 h-6 text-emerald-400" />
             </div>
+
             {/* Hero Section */}
 
-            <About />
+            <About scrollToContact={scrollToContact} scrollToProject={scrollToProject} />
+            
+            <Dock
+
+                items={items}
+
+                panelHeight={68}
+
+                baseItemSize={50}
+
+                magnification={70}
+                className='fixed z-99 bottom-8 bg-zinc-950/70 backdrop-blur-md scale-75 md:scale-100'
+
+            />
             {/* Projects Section */}
-            <Projects />
+            <Projects ref={projectRef} />
             {/*Tech Stack*/}
             <TechStack />
             <Achievements />
             {/* Contact Section */}
-            
-            <section id="contact" className="py-20 flex items-center justify-center px-4 sm:px-6 lg:px-8 bg-zinc-950 min-h-screen relative">
-                {/* Orb positioned absolutely in the background */}
-                <div className="absolute inset-0 overflow-hidden">
-                    <Orb
-                        hoverIntensity={0.5}
-                        rotateOnHover={true}
-                        hue={97}
-                        forceHoverState={true}
-                    />
-                </div>
-
-                {/* Content positioned relatively to appear above the Orb */}
-                <div className="relative z-10 max-w-5xl mx-auto text-center">
-                    <h2 className="text-3xl font-bold mb-8 text-white">Get In Touch</h2>
-                    <p className="text-gray-400 max-w-xl mx-auto mb-8">
-                        I'm currently looking for new opportunities. Whether you have a question
-                        or just want to say hi, I'll try my best to get back to you!
-                    </p>
-                    <div className="flex justify-center space-x-6">
-                        <a href="mailto:shreyansh.ind18@thapar.edu" className="text-gray-400 hover:text-emerald-400 transition duration-300">
-                            <Mail className="w-6 h-6" />
-                        </a>
-                        <a href="https://github.com/Shreyansh7676" className="text-gray-400 hover:text-emerald-400 transition duration-300">
-                            <Github className="w-6 h-6" />
-                        </a>
-                        <a href="https://www.linkedin.com/in/shreyansh-srivastava-945034257/" className="text-gray-400 hover:text-emerald-400 transition duration-300">
-                            <Linkedin className="w-6 h-6" />
-                        </a>
-                    </div>
-                </div>
-            </section>
-
-
-
+            <Contact ref={contactRef} />
         </div>
     );
 }
