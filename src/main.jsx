0f4f468f9386698509f30d6c09c36adec720ea05
Shreyansh7676 <shreyansh.ind18@gmail.com>
2025-03-30 19:18:58 +0530

    Added React router dom

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,25 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-
+import ReactDOM from 'react-dom/client';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import { useLocation } from 'react-router-dom';
 import App from './App.jsx'
-
+import Contact from './Contact.jsx'
+import Landing from './Landing.jsx'
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Landing />,
+  },
+  {
+    path: "contact",
+    element: <Contact />,
+  },
+  
+]);
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
