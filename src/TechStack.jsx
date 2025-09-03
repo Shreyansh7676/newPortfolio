35a54526f67861e6a551be4740042afdb8608a3f
Shreyansh7676 <shreyansh.ind18@gmail.com>
2025-09-03 17:42:28 +0530

    updated tech stack section

--- src/TechStack.jsx
+++ src/TechStack.jsx
@@ -1,67 +1,44 @@
 import React from 'react'
-import GlassIcons from './components/GlassIcons'
-import Photoshop from './assets/svg/Adobe Photoshop.svg'
-import ReactIcon from './assets/svg/React.svg'
-import Javascript from './assets/svg/JavaScript.svg'
-import Typescript from './assets/svg/TypeScript.svg'
-import CPlus from './assets/svg/C++ (CPlusPlus).svg'
-import Python from './assets/svg/Python.svg'
-import Git from './assets/svg/Git.svg'
-import GitHub from './assets/svg/GitHub.svg'
-import Tailwind from './assets/svg/Tailwind CSS.svg'
-import Opencv from './assets/svg/OpenCV.svg'
-import Vscode from './assets/svg/Visual Studio Code (VS Code).svg'
-import HTML from './assets/svg/HTML5.svg'
-import Vercel from './assets/svg/Vercel.svg'
-import Express from './assets/svg/express.webp'
-import Flask from './assets/svg/flask.webp'
-import Vite from './assets/svg/Vite.js.svg'
-import Unity from './assets/svg/Unity.svg'
-import C from './assets/svg/C.svg'
-import Socket from './assets/svg/Socket-io.svg.png'
-import Node from './assets/svg/node.webp'
-import CSS from './assets/svg/CSS3.svg'
-import BootStrap from './assets/svg/Bootstrap.svg'
-import Firebase from './assets/svg/Firebase.svg'
 import { Layers, Type } from 'lucide-react'
 import AnimatedContent from './components/AnimatedComponent'
 import { forwardRef } from 'react'
-import SpotlightCard from './components/SpotlightComponent'
+
+
+import LogoLoop from './components/LogoLoop'
+import { SiReact, SiJavascript, SiTypescript, SiTailwindcss, SiPython, SiExpress, SiNodedotjs, SiCss3, SiHtml5, SiFlask, SiVercel, SiSocketdotio, SiBootstrap, SiVite, SiUnity, SiOpencv, SiGithub, SiBlender } from 'react-icons/si';
+import { BiLogoFirebase } from "react-icons/bi";
+import { VscVscode } from "react-icons/vsc";
+import { TbBrandCpp } from "react-icons/tb";
+const techLogos = [
+
+    { node: <SiReact />, title: "React", href: "https://react.dev" },
+    { node: <TbBrandCpp />, title: "C++", href: "https://isocpp.org/" },
+    { node: <SiJavascript />, title: "JavaScript", href: "https://www.javascript.com/" },
+    { node: <SiExpress />, title: "Express", href: "https://expressjs.com/" },
+    { node: <SiNodedotjs />, title: "Node.js", href: "https://nodejs.org/" },
+    { node: <SiFlask />, title: "Flask", href: "https://flask.palletsprojects.com/" },
+    { node: <SiCss3 />, title: "CSS3", href: "https://www.w3.org/Style/CSS/Overview.en.html" },
+    { node: <SiHtml5 />, title: "HTML5", href: "https://www.w3.org/TR/html52/" },
+    { node: <SiGithub />, title: "GitHub", href: "https://github.com/" },
+    { node: <SiTypescript />, title: "TypeScript", href: "https://www.typescriptlang.org" },
+    { node: <SiPython />, title: "Python", href: "https://www.python.org" },
+    { node: <SiTailwindcss />, title: "Tailwind CSS", href: "https://tailwindcss.com" },
+    { node: <BiLogoFirebase />, title: "Firebase", href: "https://firebase.google.com" },
+    { node: <SiVercel />, title: "Vercel", href: "https://vercel.com" },
+    { node: <SiSocketdotio />, title: "Socket.io", href: "https://socket.io/" },
+    { node: <SiBootstrap />, title: "Bootstrap", href: "https://getbootstrap.com" },
+    { node: <SiOpencv />, title: "OpenCV", href: "https://opencv.org/" },
+    { node: <SiVite />, title: "Vite", href: "https://vitejs.dev/" },
+    { node: <SiUnity />, title: "Unity", href: "https://unity.com/" },
+    { node: <VscVscode />, title: "VS Code", href: "https://code.visualstudio.com/" },
+    { node: <BiLogoFirebase />, title: "Firebase", href: "https://firebase.google.com" },
+
+];
 
 const TechStack = forwardRef((props, ref) => {
-    const items = [
 
-        { icon: <img src={ReactIcon} alt="Photoshop" />, color: 'indigo', label: 'React' },
-        { icon: <img src={Express} alt="Photoshop" />, color: 'grey', label: 'Express' },
-
-        { icon: <img src={Javascript} alt="Photoshop" />, color: 'yellow', label: 'JavaScript' },
-        { icon: <img src={Node} alt="Photoshop" />, color: 'red', label: 'Node.js' },
-
-        { icon: <img src={Typescript} alt="Photoshop" />, color: 'blue', label: 'TypeScript' },
-
-        { icon: <img src={CPlus} alt="Photoshop" />, color: 'indigo', label: 'C++' },
-
-        { icon: <img src={Python} alt="Photoshop" />, color: 'red', label: 'Python' },
-        { icon: <img src={Flask} alt="Photoshop" />, color: 'blue', label: 'Flask' },
-
-        { icon: <img src={Git} alt="Photoshop" />, color: 'orange', label: 'Git' },
-        { icon: <img src={GitHub} alt="Photoshop" />, color: 'grey', label: 'GitHub' },
-        { icon: <img src={Tailwind} alt="Photoshop" />, color: 'indigo', label: 'Tailwind CSS' },
-        { icon: <img src={Opencv} alt="Photoshop" />, color: 'green', label: 'OpenCV' },
-        { icon: <img src={Socket} alt="Photoshop" />, color: 'yellow', label: 'Socket.io' },
-        { icon: <img src={Vscode} alt="Photoshop" />, color: 'red', label: 'VS Code' },
-        { icon: <img src={HTML} alt="Photoshop" />, color: 'orange', label: 'HTML' },
-        { icon: <img src={C} alt="Photoshop" />, color: 'blue', label: 'C' },
-        { icon: <img src={Firebase} alt="Photoshop" />, color: 'indigo', label: 'Firebase' },
-        { icon: <img src={Vercel} alt="Photoshop" />, color: 'grey', label: 'Vercel' },
-        { icon: <img src={CSS} alt="Photoshop" />, color: 'blue', label: 'CSS' },
-        { icon: <img src={BootStrap} alt="Photoshop" />, color: 'indigo', label: 'BootStrap' },
-        { icon: <img src={Vite} alt="Photoshop" />, color: 'red', label: 'Vite' },
-        { icon: <img src={Unity} alt="Photoshop" />, color: 'grey', label: 'Unity' },
-
-    ];
     return (
-        <div className='min-h-screen flex flex-col items-center justify-start' ref={ref}>
+        <div className='min-h-full flex flex-col items-center justify-start' ref={ref}>
             <AnimatedContent
 
                 distance={100}
@@ -80,18 +57,32 @@ const TechStack = forwardRef((props, ref) => {
 
                 threshold={0.1}
                 className="scroll-smooth"
-
             >
-                <h2 className="text-3xl font-right italic font-bold flex items-center justify-center">
+                {/* <h2 className="text-xl font-right italic font-bold flex items-center justify-center">
                     <Layers className="w-8 h-8 mr-4 text-emerald-400 text-center" />
                     Tech Stack & Tools
+                </h2> */}
+                <h2 className="text-4xl md:text-4xl md:pl-0 max-w-6xl mx-auto py-5 font-right italic font-bold flex items-center justify-center mb-12">
+                    <Layers className="w-10 h-10 mr-4 ml-3 text-emerald-400 text-center" />
+                    Tech Stack & Tools
                 </h2>
-                <div className='flex items-center justify-center mt-4 md:mt-20'>
+                <div className='relative w-full max-w-sm sm:max-w-lg md:max-w-2xl lg:max-w-4xl xl:max-w-6xl mx-auto overflow-hidden px-4 sm:px-6 md:px-8 mt-20 md:mt-20 mb-12'>
 
-                    <GlassIcons items={items} className="custom-class" />
+                    {/* <GlassIcons items={items} className="custom-class" /> */}
+                    <LogoLoop
+                        logos={techLogos}
+                        speed={120}
+                        direction="left"
+                        logoHeight={48}
+                        gap={40}
+                        pauseOnHover
+                        scaleOnHover
+                        fadeOut
+                        ariaLabel="Technology partners"
+                    />
 
                 </div>
-                </AnimatedContent>
+            </AnimatedContent>
         </div>
     )
 });
